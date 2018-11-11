aa6844f2a67dd3db56f8d2cd7d75ce9be389fc28
Opender Singh <opender94@gmail.com>
2018-11-11 20:38:31 +1300

    updated options.html

--- js/constants.js
+++ js/constants.js
@@ -3,12 +3,14 @@ const defaultSettings = `[
         "title": "PRODUCTION",
         "color": "#ff8000",
         "hosts": [
+            ""
         ]
     },
     {
         "title": "TEST",
         "color": "#006400",
         "hosts": [
+            ""
         ]
     }
 ]`;
