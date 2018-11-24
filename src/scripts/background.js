689c787be66d4793bd9a53e4ad9c0796d28ce8e0
Opender Singh <opender94@gmail.com>
2018-11-24 17:12:06 +1300

    Adding typescript

---
+++ src/scripts/background.js
@@ -0,0 +1,27 @@
+import { load_settings } from './settings';
+//import * as matchUrl from 'match-url-wildcard' ;
+import * as matchUrl from 'wildcard';
+
+browser.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    var url = tab.url;
+
+    load_settings().then(settingsString => {
+      var settings = JSON.parse(settingsString);
+      settings.some(function (value) {
+
+        var matched = matchUrl(value.hosts,);
+
+        if (matched) {
+          var objectToSend = { showHeader: true, label: value.label, color: value.color };
+          
+          console.info(`Matched with ${value.label}.`)
+
+          browser.tabs.sendMessage(tabId, objectToSend);
+
+          return true; // break the loop
+        }
+      })
+    });
+  }
+})
